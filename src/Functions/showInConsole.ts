a39204c0aa43780da85e487371ddeb466f01c948
Thiago Elias <thiagoelias99@gmail.com>
2023-04-03 19:54:06 -0300

    updated to check multiple files

--- src/Functions/showInConsole.ts
+++ src/Functions/showInConsole.ts
@@ -2,11 +2,12 @@ import { checkLinks } from "./checkLinks";
 export async function showInConsole(list: string | object[], file: string, check: boolean) {
     if (check) {
         if (typeof list === "object") {
+            console.log(`File: ${file}`);
             console.log(await checkLinks(list));
         } else {
             console.log(list);
         }
-    } else {
+    } else {        
         console.log(`File: ${file}`);
         console.log(list);
     }
--- src/app.ts
+++ src/app.ts
@@ -3,24 +3,26 @@ import { showInConsole } from "./Functions/showInConsole";
 import { handleError } from "./ErrorHandler/index";
 import * as fs from "fs";
 
-const path = "./arquivos/texto.md";
-const isToCheck = true;
-/* const path = process.argv[2];
-const isToCheck = process.argv[3] === "--check"; */
+const path = process.argv[2];
+const isToCheck = process.argv[3] === "check";
 
-const run = async () => {
+run();
+
+async function run() {
     try {
         if (fs.lstatSync(path).isFile()) {
             const list = await extractLinks(path);
             showInConsole(list, path, isToCheck);
 
         } else if (fs.lstatSync(path).isDirectory()) {
-            console.log("Directory");
+            const files = await fs.promises.readdir(path);
+            files.forEach(async (file) => {
+                const list = await extractLinks(`${path}/${file}`);
+                showInConsole(list, `${path}/${file}`, isToCheck);
+            });
         }
     }
     catch (error) {
         handleError(error);
     }
-};
-
-run();
+}
